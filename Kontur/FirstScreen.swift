f493afb6002418e8fa4b5dbdf903895812f25306
PRO100COD3 <vad.dz2015@yandex.ru>
2024-11-16 15:16:58 +0400

    add project and delete storyboard

---
+++ Kontur/FirstScreen.swift
@@ -0,0 +1,12 @@
+//
+//  FirstScreen.swift
+//  Kontur
+//
+//  Created by Вадим Дзюба on 16.11.2024.
+//
+
+import UIKit
+
+final class FirstScreen: UIViewController {
+
+}
---
+++ Kontur/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kontur
+//
+//  Created by Вадим Дзюба on 16.11.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

50aa97987d0bb78ec750efa4e0402bd43862140b
PRO100COD3 <vad.dz2015@yandex.ru>
2024-11-19 03:08:50 +0400

    make first screen

--- Kontur/SceneDelegate.swift
+++ Kontur/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowScene)
-        let rootViewController = FirstScreen() 
+        let firstScreenPresenter = FirstScreenPresenterImpl()
+        let rootViewController = FirstScreenViewController(presenter: firstScreenPresenter)
         window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
---
+++ Kontur/SecondScreen/SecondScreenViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SecondScreenViewController.swift
+//  Kontur
+//
+//  Created by Вадим Дзюба on 19.11.2024.
+//
+
+import Foundation
